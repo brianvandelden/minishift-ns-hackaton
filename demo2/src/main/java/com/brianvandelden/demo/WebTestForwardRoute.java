fa68120cd8f3366abbfdd0f78e99bc89a5baef32
Niek Arends <nmarends@gmail.com>
2020-09-07 16:32:48 +0200

    we have liftoff

--- demo2/src/main/java/com/brianvandelden/demo/WebTestForwardRoute.java
+++ demo2/src/main/java/com/brianvandelden/demo/WebTestForwardRoute.java
@@ -2,6 +2,7 @@ package com.brianvandelden.demo;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,13 +11,33 @@ public class WebTestForwardRoute extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         from("file://C:\\Users\\Niek Arends\\Documents\\Workspaces\\minishift-ns-hackaton\\demo2\\1")
-        .to("file://C:\\Users\\Niek Arends\\Documents\\Workspaces\\minishift-ns-hackaton\\demo2\\2");
+                .to("file://C:\\Users\\Niek Arends\\Documents\\Workspaces\\minishift-ns-hackaton\\demo2\\2");
 
-        from("timer://foo?fixedRate=true&delay=0&period=10000")
+//        from("timer://foo?fixedRate=true&delay=0&period=10000")
+//                .setHeader(Exchange.HTTP_METHOD, constant("GET"))
+//                .to("http://demo1:8080/hello")
+//                .log("${body}")
+//                .to("file://C:\\Users\\Niek Arends\\Documents\\Workspaces\\minishift-ns-hackaton\\demo2\\3\\");
+        restConfiguration()
+                .component("jetty")
+                .host("localhost")
+                .port(8080)
+                .bindingMode(RestBindingMode.auto);
+
+
+        rest("/hoi")
+                .get("/proxy").to("direct:test")
+                .get("/test").to("direct:test2");
+
+        from("direct:test2")
+                .transform().constant("hallo");
+
+        from("direct:test")
+                .log("${body}")
                 .setHeader(Exchange.HTTP_METHOD, constant("GET"))
-                .to("http://www.google.com")
-                .log("Ik doe een request")
-                .to("file://C:\\Users\\Niek Arends\\Documents\\Workspaces\\minishift-ns-hackaton\\demo2\\3\\test.txt");
+                .to("http://demo1:8080/hello?bridgeEndpoint=true");
+
+//                        .to("file://C:\\Users\\Niek Arends\\Documents\\Workspaces\\minishift-ns-hackaton\\demo2\\3\\");
     }
 
 }
