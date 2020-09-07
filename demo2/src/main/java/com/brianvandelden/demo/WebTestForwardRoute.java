3aeadb168c514fcde6957c71994a8626993782be
Niek Arends <nmarends@gmail.com>
2020-09-07 15:31:14 +0200

    we pollen

--- demo2/src/main/java/com/brianvandelden/demo/WebTestForwardRoute.java
+++ demo2/src/main/java/com/brianvandelden/demo/WebTestForwardRoute.java
@@ -1,5 +1,6 @@
 package com.brianvandelden.demo;
 
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
@@ -9,8 +10,13 @@ public class WebTestForwardRoute extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         from("file://C:\\Users\\Niek Arends\\Documents\\Workspaces\\minishift-ns-hackaton\\demo2\\1")
-
         .to("file://C:\\Users\\Niek Arends\\Documents\\Workspaces\\minishift-ns-hackaton\\demo2\\2");
+
+        from("timer://foo?fixedRate=true&delay=0&period=10000")
+                .setHeader(Exchange.HTTP_METHOD, constant("GET"))
+                .to("http://www.google.com")
+                .log("Ik doe een request")
+                .to("file://C:\\Users\\Niek Arends\\Documents\\Workspaces\\minishift-ns-hackaton\\demo2\\3\\test.txt");
     }
 
 }
