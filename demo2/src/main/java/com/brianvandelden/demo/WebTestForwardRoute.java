2844b3744743dd1655eb320fe23a98f1782cd21a
Niek Arends <nmarends@gmail.com>
2020-09-07 16:40:04 +0200

    haksdhkjslhkaj

--- demo2/src/main/java/com/brianvandelden/demo/WebTestForwardRoute.java
+++ demo2/src/main/java/com/brianvandelden/demo/WebTestForwardRoute.java
@@ -35,7 +35,7 @@ public class WebTestForwardRoute extends RouteBuilder {
         from("direct:test")
                 .log("${body}")
                 .setHeader(Exchange.HTTP_METHOD, constant("GET"))
-                .to("http://demo1:8080/hello?bridgeEndpoint=true");
+                .to("http://minishift-ns-hackaton:8080/hello?bridgeEndpoint=true");
 
 //                        .to("file://C:\\Users\\Niek Arends\\Documents\\Workspaces\\minishift-ns-hackaton\\demo2\\3\\");
     }
