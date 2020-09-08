266a48fb22f99be080f2ec5f0c5b45e020427c86
Niek Arends <nmarends@gmail.com>
2020-09-08 15:29:09 +0200

    Demo2 netjes gemaakt

--- demo2/src/main/java/com/brianvandelden/demo/WebTestForwardRoute.java
+++ demo2/src/main/java/com/brianvandelden/demo/WebTestForwardRoute.java
@@ -10,14 +10,6 @@ public class WebTestForwardRoute extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        from("file://C:\\Users\\Niek Arends\\Documents\\Workspaces\\minishift-ns-hackaton\\demo2\\1")
-                .to("file://C:\\Users\\Niek Arends\\Documents\\Workspaces\\minishift-ns-hackaton\\demo2\\2");
-
-//        from("timer://foo?fixedRate=true&delay=0&period=10000")
-//                .setHeader(Exchange.HTTP_METHOD, constant("GET"))
-//                .to("http://demo1:8080/hello")
-//                .log("${body}")
-//                .to("file://C:\\Users\\Niek Arends\\Documents\\Workspaces\\minishift-ns-hackaton\\demo2\\3\\");
         restConfiguration()
                 .component("jetty")
 //                .host("localhost")
@@ -36,8 +28,6 @@ public class WebTestForwardRoute extends RouteBuilder {
                 .log("${body}")
                 .setHeader(Exchange.HTTP_METHOD, constant("GET"))
                 .to("http://minishift-ns-hackaton:8080/hello?bridgeEndpoint=true");
-
-//                        .to("file://C:\\Users\\Niek Arends\\Documents\\Workspaces\\minishift-ns-hackaton\\demo2\\3\\");
     }
 
 }
